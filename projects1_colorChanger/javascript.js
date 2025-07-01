536490a21d9cc5126a249680d97dfe820b40f5b8
itsmeprachii <149767095+itsmeprachii@users.noreply.github.com>
2025-07-01 16:50:22 +0000

    color Change

---
+++ projects1_colorChanger/javascript.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+buttons.forEach(function(button) {
+    button.addEventListener('click', function(e) {
+        if (e.target.id === 'black') {
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id === 'blue') {
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id === 'white') {
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id === 'red') {
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id === 'pink') {
+            body.style.backgroundColor = e.target.id
+        }
+    })
+
+})
